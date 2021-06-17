0d427078f79d4189e7d10cb92f94a494b62f9953
kennethhowell <howell.kenneth.a@gmail.com>
2021-06-17 07:40:01 -0500

    morning commit - adding many to many example

--- src/main/java/com/codeup/demoblog/models/Ad.java
+++ src/main/java/com/codeup/demoblog/models/Ad.java
@@ -2,31 +2,63 @@ package com.codeup.demoblog.models;
 
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
-@Table( name= "ads")
+@Table(name = "ads")
 public class Ad {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @Column(nullable = false, length = 100)
+    @Column(nullable = false, length = 200)
     private String title;
 
     @Column(nullable = false)
     private String description;
 
-    public Ad() {
+    @OneToOne
+    private User owner;
 
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "ad")
+    private List<AdImage> images;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name="ads_categories",
+            joinColumns={@JoinColumn(name="ad_id")},
+            inverseJoinColumns={@JoinColumn(name="category_id")}
+    )
+    private List<AdCategory> categories;
+
+    // spring framework uses this empty constructor
+    public Ad() {}
+
+    // Insert constructor doesn't require an ID.
+    public Ad(String title, String description, User owner, List<AdImage> images, List<AdCategory> categories) {
+        this.title = title;
+        this.description = description;
+        this.owner = owner;
+        this.images = images;
+        this.categories = categories;
     }
 
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
+    // Read constructor
+    public Ad(long id, String title, String description, User owner, List<AdImage> images, List<AdCategory> categories) {
         this.id = id;
+        this.title = title;
+        this.description = description;
+        this.owner = owner;
+        this.images = images;
+        this.categories = categories;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public String getTitle() {
@@ -37,11 +69,35 @@ public class Ad {
         this.title = title;
     }
 
-    public String getDescription() {
-        return description;
+    public long getId() {
+        return id;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public List<AdImage> getImages() {
+        return images;
+    }
+
+    public void setImages(List<AdImage> images) {
+        this.images = images;
+    }
+
+    public List<AdCategory> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<AdCategory> categories) {
+        this.categories = categories;
     }
 }
